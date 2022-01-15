c88f66e90020edae0e17ee1b21b2e74766e4ca65
matthewhirai <mhirai301@gmail.com>
2022-01-15 14:40:16 -0800

    Update social.js

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,26 +1,26 @@
-import Instagram from '../../assets/icons/instagram.png';
-import GitHub from '../../assets/icons/linkedin.png';
-import LinkedIn from '../../assets/icons/github.png';
-import Email from '../../assets/icons/email.png';
+import Instagram from "../../assets/icons/instagram.png";
+import GitHub from "../../assets/icons/github.png";
+import LinkedIn from "../../assets/icons/linkedin.png";
+import Email from "../../assets/icons/email.png";
 export const SocialData = [
 	{
-		platform: 'Instagram',
-		link: 'https://www.instagram.com/_m.hirai/',
+		platform: "Instagram",
+		link: "https://www.instagram.com/_m.hirai/",
 		icon: Instagram
 	},
 	{
-		platform: 'GitHub',
-		link: 'https://github.com/matthewhirai',
+		platform: "GitHub",
+		link: "https://github.com/matthewhirai",
 		icon: GitHub
 	},
 	{
-		platform: 'LinkedIn',
-		link: 'https://www.linkedin.com/in/matthew-hirai/',
+		platform: "LinkedIn",
+		link: "https://www.linkedin.com/in/matthew-hirai/",
 		icon: LinkedIn
 	},
 	{
-		platform: 'mail',
-		link: 'mailto:mhirai301@gmail.com',
+		platform: "mail",
+		link: "mailto:mhirai301@gmail.com",
 		icon: Email
 	}
 ];
